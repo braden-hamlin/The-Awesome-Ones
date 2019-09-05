1be2aaa17badbbe5f7c7fc307cac09a51b254c94
Braden Hamlin <braden@placefull.com>
2019-09-05 16:51:31 -0700

    Quiz #2

--- The Awesome Ones/ViewController.swift
+++ The Awesome Ones/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var viewer: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    var randomStart = Int.random(in: 0...7)
+    var people = ["Ethan",
+                   "Braden",
+                   "Kyle",
+                   "Reece",
+                   "Max",
+                   "Jessica",
+                   "Sophia",
+                   "Jared"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
-
+    @IBAction func button(_ sender: UIButton) {
+        if randomStart > people.count-1 {
+            randomStart = 0
+        }
+        label.text = people[randomStart]
+        viewer.image = UIImage(named: "\(randomStart)")
+        randomStart += 1
+    }
 }
 
